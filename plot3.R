b226d0df8638d40e4b43ea26535ad280cfa6df20
Dicko Ahmadou <dicko.ahmadou@gmail.com>
2014-05-09 22:24:00 +0000

    small modifications

--- plot3.R
+++ plot3.R
@@ -1,5 +1,7 @@
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
 if (!file.exists("data/household_power_consumption.txt")) {
+    dir.create("data")
     download.file(url, "data/householdpowconsump.zip", method = "curl")
     unzip("data/householdpowconsump.zip", exdir = "data")
     file.remove("data/householdpowconsump.zip")
